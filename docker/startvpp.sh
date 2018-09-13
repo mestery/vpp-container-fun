e065748de80fdbe24e99177e201fb5917d138687
Kyle Mestery <mestery@mestery.com>
2018-09-13 14:48:20 -0500

    Additional debug
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- docker/startvpp.sh
+++ docker/startvpp.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -xe
+
 ip link add name vpp1out type veth peer name vpp1host
 ip link set dev vpp1out up
 ip link set dev vpp1host up
 ip addr add 10.10.1.1/24 dev vpp1host
 ip route add 10.10.2.0/24 via 10.10.1.2
+ip a
+cat /etc/vpp/config1.txt
+cat /etc/vpp/config2.txt
 
 # Run the VPP daemons
 /usr/bin/vpp -c /etc/vpp/startup1.conf
