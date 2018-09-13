9530af46ba6673405c8db6f10f4c894541fecd15
Kyle Mestery <mestery@mestery.com>
2018-09-13 15:20:16 -0500

    Shift configuration around a bit
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- docker/startvpp.sh
+++ docker/startvpp.sh
@@ -2,15 +2,32 @@
 
 set -xe
 
+# Run the VPP daemons
+/usr/bin/vpp -c /etc/vpp/startup1.conf
+/usr/bin/vpp -c /etc/vpp/startup2.conf
+
+# Setup the host link
 ip link add name vpp1out type veth peer name vpp1host
 ip link set dev vpp1out up
 ip link set dev vpp1host up
 ip addr add 10.10.1.1/24 dev vpp1host
 ip route add 10.10.2.0/24 via 10.10.1.2
-ip a
-cat /etc/vpp/config1.txt
-cat /etc/vpp/config2.txt
 
-# Run the VPP daemons
-/usr/bin/vpp -c /etc/vpp/startup1.conf
-/usr/bin/vpp -c /etc/vpp/startup2.conf
+# Make sure VPP is *really* running
+typeset -i cnt=60
+until ls -l /run/vpp/cli-vpp1.sock ; do
+	((cnt=cnt-1)) || return 1
+	sleep 1
+done
+typeset -i cnt=60
+until ls -l /run/vpp/cli-vpp2.sock ; do
+	((cnt=cnt-1)) || return 1
+	sleep 1
+done
+
+vppctl -s /run/vpp/cli-vpp1.sock create host-interface name vpp1out
+vppctl -s /run/vpp/cli-vpp1.sock set int state host-vpp1out up
+vppctl -s /run/vpp/cli-vpp1.sock set int ip address host-vpp1out 10.10.1.2/24
+
+# We do not want to exit, so ...
+tail -f /dev/null
