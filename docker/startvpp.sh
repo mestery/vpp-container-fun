022fd2f95e06f7d9a822e7a6fa6d0a07ddff9c4c
Kyle Mestery <mestery@mestery.com>
2018-09-13 12:46:16 -0500

    Pre-build some routing into the containers
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- docker/startvpp.sh
+++ docker/startvpp.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+ip link add name vpp1out type veth peer name vpp1host
+ip link set dev vpp1out up
+ip link set dev vpp1host up
+ip addr add 10.10.1.1/24 dev vpp1host
+ip route add 10.10.2.0/24 via 10.10.1.2
+
+# Run the VPP daemons
 /usr/bin/vpp -c /etc/vpp/startup1.conf
 /usr/bin/vpp -c /etc/vpp/startup2.conf
